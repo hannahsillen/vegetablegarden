62e5bfb6ba0fcbf3c1c54d7ed7c07cc563a0c714
hannahsillen <90184137+hannahsillen@users.noreply.github.com>
2022-06-08 13:37:42 +0200

    Add files via upload

--- garden.js
+++ garden.js
@@ -1,106 +1,106 @@
 const getYieldForPlant = (plant, factor) => {
-    if(factor.sun == "low"){
-        if(factor.wind == "low"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.low)
+    if(factor.sun === "low"){
+        if(factor.wind === "low"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.low)
             }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.medium)
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.medium)
             }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.high)
-            }
-        }
-        else if(factor.wind == "medium"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.low)
-            }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.medium)
-            }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.high)
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.high)
             }
         }
-        else if(factor.wind == "high"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.low)
+        else if(factor.wind === "medium"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.low)
             }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.medium)
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.medium)
             }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.low) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.high)
-            }
-        }
-    }
-    else if(factor.sun == "medium"){
-        if(factor.wind == "low"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.low)
-            }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.medium)
-            }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.high)
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.high)
             }
         }
-        else if(factor.wind == "medium"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.low)
+        else if(factor.wind === "high"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.low)
             }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.medium)
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.medium)
             }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.high)
-            }
-        }
-        else if(factor.wind == "high"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.low)
-            }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.medium)
-            }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.medium) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.high)
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.low) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.high)
             }
         }
     }
-    else if(factor.sun == "high"){
-        if(factor.wind == "low"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.low)
+    else if(factor.sun === "medium"){
+        if(factor.wind === "low"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.low)
             }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.medium)
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.medium)
             }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.low) * (100 + plant.factor.rain.high)
-            }
-        }
-        else if(factor.wind == "medium"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.low)
-            }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.medium)
-            }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.medium) * (100 + plant.factor.rain.high)
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.high)
             }
         }
-        else if(factor.wind == "high"){
-            if(factor.rain == "low"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.low)
+        else if(factor.wind === "medium"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.low)
             }
-            else if(factor.rain == "medium"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.medium)
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.medium)
             }
-            else if(factor.rain == "high"){
-                return plant.yield * (100 + plant.factor.sun.high) * (100 + plant.factor.wind.high) * (100 + plant.factor.rain.high)
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.high)
+            }
+        }
+        else if(factor.wind === "high"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.low)
+            }
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.medium)
+            }
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.medium) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.high)
+            }
+        }
+    }
+    else if(factor.sun === "high"){
+        if(factor.wind === "low"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.low)
+            }
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.medium)
+            }
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.low) * (1 + plant.factor.rain.high)
+            }
+        }
+        else if(factor.wind === "medium"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.low)
+            }
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.medium)
+            }
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.medium) * (1 + plant.factor.rain.high)
+            }
+        }
+        else if(factor.wind === "high"){
+            if(factor.rain === "low"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.low)
+            }
+            else if(factor.rain === "medium"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.high) * (1 + plant.factor.rain.medium)
+            }
+            else if(factor.rain === "high"){
+                return plant.yield * (1 + plant.factor.sun.high) * (1 + plant.factor.wind.high) * (100 + plant.factor.rain.high)
             }
         }
     }
--- garden.test.js
+++ garden.test.js
@@ -13,19 +13,19 @@ const corn = {
     saleprice: 2,
     factor: {
         sun: {
-        low: -50,
+        low: -0.50,
         medium: 0,
-        high: 50,
+        high: 0.50,
         },
         wind: {
         low: 0,
-        medium: -20,
-        high: -50
+        medium: -0.20,
+        high: -0.50
         },
         rain: {
-        low: -100,
+        low: -1.00,
         medium: 0,
-        high: -50
+        high: -0.50
         }
     },
 };
@@ -36,19 +36,19 @@ const pumpkin = {
     saleprice: 3,
     factor: {
         sun: {
-        low: -50,
+        low: -0.50,
         medium: 0,
-        high: 50,
+        high: 0.50,
         },
         wind: {
         low: 0,
         medium: 0,
-        high: -10
+        high: -0.10
         },
         rain: {
-        low: -100,
+        low: -1.00,
         medium: 0,
-        high: -50
+        high: -0.50
         }
     },
 };
@@ -68,42 +68,42 @@ const input = {
 
 describe("getYieldForPlant", () => {
     test("Get yield for plant with environment factors", () => {
-        expect(getYieldForPlant(corn, environmentFactors)).toBeTruthy();
+        expect(getYieldForPlant(corn, environmentFactors)).toBeCloseTo(2.4);
     });
 });
 
 describe("getYieldForCrop", () => {
-    test("Get yield for crop, simple", () => {
-        expect(getYieldForCrop(input, environmentFactors)).toBeTruthy();
+    test("Get yield for crop", () => {
+        expect(getYieldForCrop(input, environmentFactors)).toBeCloseTo(24);
     });
 });
 
 describe("getTotalYield", () => {
     test("Calculate total yield with multiple crops", () => {
-        expect(getTotalYield({ crops }, environmentFactors)).toBeTruthy();
+        expect(getTotalYield({ crops }, environmentFactors)).toBeCloseTo(20);
     });
 });
 
 describe("getCostForCrop", () => {
     test("get the total cost for the crop", () => {
-        expect(getCostForCrop(input, environmentFactors)).toBeTruthy();
+        expect(getCostForCrop(input, environmentFactors)).toBeCloseTo(24);
     });
 });
 
 describe("getRevenueForCrop", () => {
     test("get the total revenue of the crops", () => {
-        expect(getRevenueForCrop(input, environmentFactors)).toBeTruthy();
+        expect(getRevenueForCrop(input, environmentFactors)).toBeCloseTo(48);
     });
 });
 
 describe("getProfitForCrop", () => {
     test("get the total profit of the crops", () => {
-        expect(getProfitForCrop(input, environmentFactors)).toBeTruthy();
+        expect(getProfitForCrop(input, environmentFactors)).toBeCloseTo(24);
     });
 });
 
 describe("getTotalProfit", () => {
     test("Calculate total profit with multiple crops", () => {
-        expect(getTotalProfit({ crops }, environmentFactors)).toBeTruthy();
+        expect(getTotalProfit({ crops }, environmentFactors)).toBeCloseTo(28);
     });
 });
